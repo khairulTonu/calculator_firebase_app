01c8ea9b433954f4af21481c2c1b1a3c6513e089
tonu127 <khairul.islam@vivacomsolutions.com>
2021-05-30 12:25:27 +0600

    debug banner has been hidden.

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class CalculatorApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return MaterialApp(
+			debugShowCheckedModeBanner: false,
 			theme: ThemeData(primarySwatch: Colors.teal),
 			home: getCurrentUser() != null? Calculator() : SignIn(),
 		);
